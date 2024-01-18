ebc6daeda186d7e3880bc9369661e3b851dc3f1e
Kostov Ilya <kick1991@mail.ru>
2024-01-19 01:12:01 +0500

    update admin trading_network and users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Отображение пользователя в административной панели."""
+    list_display = ('id', 'email', 'is_active', 'groups_display')
+    filter_horizontal = ('groups', 'user_permissions',)
+
+    def groups_display(self, obj):
+        groups = obj.groups.all()
+        if groups:
+            return ', '.join([group.name for group in groups])
+        else:
+            return 'Не состоит в группах'
+
+    groups_display.short_description = 'Группы'
