f98fb5ae9bea366a8fcff53823088efa3f2b91c6
Kostov Ilya <kick1991@mail.ru>
2024-01-19 01:34:18 +0500

    add command create superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,22 @@
+import os
+
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Create superuser'
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='kenaa@example.com',
+            # email=os.getenv('SUPERUSER_EMAIL'),
+            first_name='admin',
+            is_staff=True,
+            is_active=True,
+            is_superuser=True,
+        )
+        # user.set_password(os.getenv('SUPERUSER_PASSWORD'))
+        user.set_password('123')
+        user.save()
